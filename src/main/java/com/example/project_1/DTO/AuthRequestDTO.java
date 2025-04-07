9ab2ad8ae614ffe9ca5498c6e7b46500ab2e46cb
pranjalbansal1903 <pranjalbansal66@gmail.com>
2025-04-07 10:07:10 +0530

    auth request DTO

---
+++ src/main/java/com/example/project_1/DTO/AuthRequestDTO.java
@@ -0,0 +1,12 @@
+package com.example.project_1.DTO;
+
+public class AuthRequestDTO {
+    private String username;
+    private String password;
+
+    public String getUsername() { return username; }
+    public void setUsername(String username) { this.username = username; }
+
+    public String getPassword() { return password; }
+    public void setPassword(String password) { this.password = password; }
+}
